041cc8fc04423913919f9a67d7e7379682815e03
lee0806 <leeseh0806@gmail.com>
2026-03-18 16:17:07 +0900

    [INIT] 프로젝트 초기 세팅
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

---
+++ src/main/java/com/example/goormtodoback/GoormTodoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.goormtodoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormTodoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormTodoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goormtodoback/GoormTodoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goormtodoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormTodoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
