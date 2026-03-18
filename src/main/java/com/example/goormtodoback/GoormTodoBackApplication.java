ec8ce0c0913e6c934022d9cc08d95bd75d9f6eee
lee0806 <leeseh0806@gmail.com>
2026-03-18 17:02:51 +0900

    [INIT] DB INIT
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/main/java/com/example/goormtodoback/GoormTodoBackApplication.java
+++ src/main/java/com/example/goormtodoback/GoormTodoBackApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoormTodoBackApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GoormTodoBackApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(GoormTodoBackApplication.class, args);
+	}
 }
