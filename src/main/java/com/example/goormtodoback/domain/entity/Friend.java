fdfaa058119bd8813a4f27347a20f0db805fdc48
lee0806 <leeseh0806@gmail.com>
2026-03-20 13:59:58 +0900

    [FEAT] friend repository 생성
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/main/java/com/example/goormtodoback/domain/entity/Friend.java
+++ src/main/java/com/example/goormtodoback/domain/entity/Friend.java
@@ -9,29 +9,33 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "friend",
+// 친구 관계를 나타내는 Entity
+
+
+@Entity // JPA 엔티티
+@Table(name = "friend", // 테이블 이름
 uniqueConstraints = {
-@UniqueConstraint(columnNames = {"request_id", "receive_id"})
+@UniqueConstraint(columnNames = {"request_id", "receive_id"}) // 유니크 제약
 })
 @Getter
 @NoArgsConstructor
 public class Friend {
 
-    @Id
+    @Id // PK 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "request_id", nullable = false)
+    @JoinColumn(name = "request_id", nullable = false) // request_id라는 컬럼으로 저장
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User request;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "receive_id", nullable = false)
+    @JoinColumn(name = "receive_id", nullable = false) // receive_id라는 컬럼으로 저장
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User receive;
 
+    // 친구 요청 상태
     @Enumerated(EnumType.STRING)
     @Column(length = 20, nullable = false)
     private FriendStatus status = FriendStatus.PENDING;
