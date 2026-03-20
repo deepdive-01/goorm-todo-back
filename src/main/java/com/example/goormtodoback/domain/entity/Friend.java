2ae91b390691d6bfbbf96a6623414f6543a578b8
lee0806 <leeseh0806@gmail.com>
2026-03-20 13:45:09 +0900

    [FEAT] Friend entity 정의
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/main/java/com/example/goormtodoback/domain/entity/Friend.java
+++ src/main/java/com/example/goormtodoback/domain/entity/Friend.java
@@ -1,4 +1,60 @@
 package com.example.goormtodoback.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "friend",
+uniqueConstraints = {
+@UniqueConstraint(columnNames = {"request_id", "receive_id"})
+})
+@Getter
+@NoArgsConstructor
 public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "request_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User request;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receive_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User receive;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private FriendStatus status = FriendStatus.PENDING;
+
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    public enum FriendStatus {
+        PENDING, ACCEPTED, REJECTED
+    }
+
+    @Builder
+    public Friend(User request, User receive) {
+        this.request = request;
+        this.receive = receive;
+        this.createAt = LocalDateTime.now();
+    }
+
+    public void accept() {
+        this.status = FriendStatus.ACCEPTED;
+    }
+
+    public void reject() {
+        this.status = FriendStatus.REJECTED;
+    }
 }
---
+++ src/main/java/com/example/goormtodoback/repository/FriendRepository.java
@@ -0,0 +1,4 @@
+package com.example.goormtodoback.repository;
+
+public class FriendRepository {
+}
